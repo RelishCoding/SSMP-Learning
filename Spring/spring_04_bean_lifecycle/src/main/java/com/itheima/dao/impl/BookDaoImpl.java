cc2ef10648e99dfd4c13fd8608d83f14b57b27d0
qzh <2806894439@qq.com>
2024-05-20 12:41:15 +0800

    提供bean的初始化和销毁方法

---
+++ Spring/spring_04_bean_lifecycle/src/main/java/com/itheima/dao/impl/BookDaoImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.BookDao;
+
+public class BookDaoImpl implements BookDao {
+    @Override
+    public void save() {
+        System.out.println("book dao save ...");
+    }
+
+    // bean初始化对应的操作
+    public void init() {
+        System.out.println("init ...");
+    }
+
+    // bean销毁前对应的操作
+    public void destroy() {
+        System.out.println("destroy ...");
+    }
+}
